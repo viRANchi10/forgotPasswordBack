94b4e38b7035c3217292b41609e292e29cf2eeef
viRANchi10 <mp125205@gmail.com>
2023-04-24 20:49:06 +0530

    changed

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/forgotpass", {
+  .connect(process.env.DB, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
